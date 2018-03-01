996753620097415cdfdc60a5d14ec1a9312a2f8b
jordan luce <jordan.luce972@gmail.com>
2018-03-01 17:05:57 +0000

    edit and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,25 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end 
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to root_path
+	end
+
 	def show
 		@post = Post.find(params[:id])
 	end
